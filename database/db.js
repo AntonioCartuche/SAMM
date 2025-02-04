05cd86a4e4290aa49af9035d6697439f042857df
Antonio.C <jose.cartuche.r@unl.edu.ec>
2025-02-04 15:51:12 -0500

    coniguracion de DB

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: 'sammdb.mysql.database.azure.com',
+  user: 'antonio',
+  password: 'tonio689@',
+  database: 'sammdb'
 });
+
 
 connection.connect((error)=>{
     if(error){
