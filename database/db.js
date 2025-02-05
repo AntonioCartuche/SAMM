a24b4345aabd41b544511ad108d0a5481c778deb
Antonio.C <jose.cartuche.r@unl.edu.ec>
2025-02-04 21:46:36 -0500

    coniguracion de DB 8

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mysql = require('mysql2');
 
-
+/*
 const connection = mysql.createConnection({
   host: 'sammdb.mysql.database.azure.com',
   user: 'antonio',
@@ -11,7 +11,15 @@ const connection = mysql.createConnection({
   port: 3306,
 });
 
+*/
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
 
 connection.connect((error)=>{
     if(error){
